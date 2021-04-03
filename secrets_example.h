7b73529d94b86232ae8ab6c66df2b6de5fe64b31
Julian Rendell <juliangrendell@gmail.com>
2021-04-03 16:37:43 -0700

    adding example secrets file

---
+++ secrets_example.h
@@ -0,0 +1,15 @@
+#ifndef secrets_h
+#define secrets_h
+
+#
+# COPY THIS FILE TO secrets.h
+# And modify as needed.
+# Make sure it's in your .gitignore, and 
+# don't accidentally publish it to GitHub etc!
+#
+
+const string SSID = "ssid name";
+const string SSIDPSK = "ssid psk";
+
+#endif
+
